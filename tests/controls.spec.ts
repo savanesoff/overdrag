975a2c5c82e180521513c2eec53a3778ceab43c2
Sam Avanesov <sam.avanesoff@gmail.com>
2023-06-24 10:21:46 -0400

    Right controls tests added

--- tests/controls.spec.ts
+++ tests/controls.spec.ts
@@ -381,4 +381,197 @@ describe("controls", () => {
       expect(overdrag.element.style.cursor).toBe(Overdrag.CURSOR.OVER);
     });
   });
+
+  describe("When mouse is over right control sensor", () => {
+    let overdrag: Overdrag;
+    let emitSpy: jest.SpyInstance;
+    let attrSpy: jest.SpyInstance;
+    beforeEach(() => {
+      overdrag = createInstance();
+      emitSpy = jest.spyOn(overdrag, "emit");
+      attrSpy = jest.spyOn(overdrag.element, "setAttribute");
+      overdrag.element.dispatchEvent(new MouseEvent("mouseenter"));
+    });
+    beforeEach(() => {
+      // Reset mock function calls
+      jest.clearAllMocks();
+    });
+
+    it(`should activate sensor at edge`, () => {
+      moveElementCursor(overdrag, {
+        x: overdrag.position.visualBounds.width,
+        y: 0,
+      });
+
+      expect(overdrag.controls.right).toBe(true);
+    });
+
+    it(`should activate sensor at end of zone`, () => {
+      moveElementCursor(overdrag, {
+        x: overdrag.position.visualBounds.width - overdrag.controlsThreshold,
+        y: 0,
+      });
+
+      expect(overdrag.controls.right).toBe(true);
+    });
+
+    it(`should not activate sensor before edge`, () => {
+      moveElementCursor(overdrag, {
+        x: overdrag.position.visualBounds.width + 1,
+        y: 0,
+      });
+
+      expect(overdrag.controls.right).toBe(false);
+    });
+
+    it(`should not activate sensor after end of zone`, () => {
+      moveElementCursor(overdrag, {
+        x:
+          overdrag.position.visualBounds.width - overdrag.controlsThreshold - 1,
+        y: 0,
+      });
+
+      expect(overdrag.controls.right).toBe(false);
+    });
+
+    it(`should not activate sensor if vertical outside of bounds`, () => {
+      moveElementCursor(overdrag, {
+        x: 0,
+        y: -1,
+      });
+      expect(overdrag.controls.right).toBe(false);
+
+      moveElementCursor(overdrag, {
+        x: 0,
+        y: overdrag.position.visualBounds.height + 1,
+      });
+      expect(overdrag.controls.right).toBe(false);
+    });
+
+    it(`should set "${Overdrag.ATTRIBUTES.CONTROLS}" attribute to "right"' if only right sensor activated`, () => {
+      moveElementCursor(overdrag, {
+        x: overdrag.position.visualBounds.width,
+        y: overdrag.controlsThreshold + 1,
+      });
+
+      expect(attrSpy).toHaveBeenCalledWith(
+        Overdrag.ATTRIBUTES.CONTROLS,
+        "right"
+      );
+    });
+
+    it(`should set "${Overdrag.ATTRIBUTES.CONTROLS}" attribute to "right-top" for both sensors activation`, () => {
+      moveElementCursor(overdrag, {
+        x: overdrag.position.visualBounds.width,
+        y: 0,
+      });
+
+      expect(attrSpy).toHaveBeenCalledWith(
+        Overdrag.ATTRIBUTES.CONTROLS,
+        "right-top"
+      );
+    });
+
+    it(`should set "${Overdrag.ATTRIBUTES.CONTROLS}" attribute to "right-bottom" for both sensors activation`, () => {
+      moveElementCursor(overdrag, {
+        x: overdrag.position.visualBounds.width,
+        y: overdrag.position.visualBounds.height,
+      });
+
+      expect(attrSpy).toHaveBeenCalledWith(
+        Overdrag.ATTRIBUTES.CONTROLS,
+        "right-bottom"
+      );
+    });
+
+    it(`should emit "${Overdrag.EVENTS.CONTROLS_ACTIVE}" event`, () => {
+      moveElementCursor(overdrag, {
+        x: overdrag.position.visualBounds.width,
+        y: overdrag.controlsThreshold + 1,
+      });
+
+      expect(emitSpy).toHaveBeenCalledWith(
+        Overdrag.EVENTS.CONTROLS_ACTIVE,
+        overdrag
+      );
+    });
+
+    it(`should emit "${Overdrag.EVENTS.CONTROLS_INACTIVE}" event once left sensor is deactivated`, () => {
+      moveElementCursor(overdrag, {
+        x: overdrag.position.visualBounds.width,
+        y: overdrag.controlsThreshold + 1,
+      });
+
+      moveElementCursor(overdrag, {
+        x: overdrag.position.visualBounds.width + 1,
+        y: overdrag.controlsThreshold + 1,
+      });
+
+      expect(emitSpy).toHaveBeenCalledTimes(2);
+      expect(emitSpy).toHaveBeenCalledWith(
+        Overdrag.EVENTS.CONTROLS_INACTIVE,
+        overdrag
+      );
+    });
+
+    it(`should emit "${Overdrag.EVENTS.CONTROLS_INACTIVE}" for out of bound vertical`, () => {
+      moveElementCursor(overdrag, {
+        x: overdrag.position.visualBounds.width,
+        y: overdrag.controlsThreshold + 1,
+      });
+
+      moveElementCursor(overdrag, {
+        x: overdrag.position.visualBounds.width,
+        y: -1,
+      });
+
+      expect(emitSpy).toHaveBeenCalledTimes(2);
+      expect(emitSpy).toHaveBeenCalledWith(
+        Overdrag.EVENTS.CONTROLS_INACTIVE,
+        overdrag
+      );
+    });
+
+    it(`should set element cursor to "${Overdrag.CURSOR.RIGHT}"`, () => {
+      moveElementCursor(overdrag, {
+        x: overdrag.position.visualBounds.width,
+        y: overdrag.controlsThreshold + 1,
+      });
+
+      expect(overdrag.element.style.cursor).toBe(Overdrag.CURSOR.RIGHT);
+    });
+
+    it(`should set element cursor to "${Overdrag.CURSOR.RIGHT_TOP}"`, () => {
+      moveElementCursor(overdrag, {
+        x: overdrag.position.visualBounds.width,
+        y: 0,
+      });
+
+      expect(overdrag.element.style.cursor).toBe(Overdrag.CURSOR.RIGHT_TOP);
+    });
+
+    it(`should set element cursor to "${Overdrag.CURSOR.RIGHT_BOTTOM}"`, () => {
+      moveElementCursor(overdrag, {
+        x: overdrag.position.visualBounds.width,
+        y: overdrag.position.visualBounds.height,
+      });
+
+      expect(overdrag.element.style.cursor).toBe(Overdrag.CURSOR.RIGHT_BOTTOM);
+    });
+
+    it(`should set element cursor to "${Overdrag.CURSOR.OVER}"`, () => {
+      moveElementCursor(overdrag, {
+        x: overdrag.position.visualBounds.width,
+        y: overdrag.controlsThreshold + 1,
+      });
+
+      moveElementCursor(overdrag, {
+        x:
+          overdrag.position.visualBounds.width - overdrag.controlsThreshold - 1,
+        y: overdrag.controlsThreshold + 1,
+      });
+
+      expect(overdrag.element.style.cursor).toBe(Overdrag.CURSOR.OVER);
+    });
+  });
 });

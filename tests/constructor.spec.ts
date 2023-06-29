45d4571565e70602d30014b8b79ab2329849c878
Sam Avanesov <sam.avanesoff@gmail.com>
2023-06-29 13:20:23 -0400

    Adds missing constructor specs

--- tests/constructor.spec.ts
+++ tests/constructor.spec.ts
@@ -21,57 +21,133 @@ describe("Constructor", () => {
     );
   });
 
-  it("should set DEFAULTS if no props are provided", () => {
-    const overdrag = createInstance({
-      minContentHeight: undefined as any,
-      minContentWidth: undefined as any,
-      snapThreshold: undefined as any,
-      controlsThreshold: undefined as any,
-      clickDetectionThreshold: undefined as any,
-    });
+  it(`should set default "minContentHeight"`, () => {
+    const overdrag = createInstance({ minContentHeight: undefined });
     expect(overdrag.minContentHeight).toBe(Overdrag.DEFAULTS.minContentHeight);
+  });
+
+  it(`should set default "minContentWidth"`, () => {
+    const overdrag = createInstance({ minContentWidth: undefined });
     expect(overdrag.minContentWidth).toBe(Overdrag.DEFAULTS.minContentWidth);
+  });
+
+  it(`should set default "maxContentHeight"`, () => {
+    const overdrag = createInstance({ maxContentHeight: undefined });
+    expect(overdrag.maxContentHeight).toBe(Overdrag.DEFAULTS.maxContentHeight);
+  });
+
+  it(`should set default "maxContentWidth"`, () => {
+    const overdrag = createInstance({ maxContentWidth: undefined });
+    expect(overdrag.maxContentWidth).toBe(Overdrag.DEFAULTS.maxContentWidth);
+  });
+
+  it(`should set default "snapThreshold"`, () => {
+    const overdrag = createInstance({ snapThreshold: undefined });
     expect(overdrag.snapThreshold).toBe(Overdrag.DEFAULTS.snapThreshold);
+  });
+
+  it(`should set default "controlsThreshold"`, () => {
+    const overdrag = createInstance({ controlsThreshold: undefined });
     expect(overdrag.controlsThreshold).toBe(
       Overdrag.DEFAULTS.controlsThreshold
     );
+  });
+
+  it(`should set default "clickDetectionThreshold"`, () => {
+    const overdrag = createInstance({ clickDetectionThreshold: undefined });
     expect(overdrag.clickDetectionThreshold).toBe(
       Overdrag.DEFAULTS.clickDetectionThreshold
     );
+  });
+
+  it(`should set default "stack"`, () => {
+    const overdrag = createInstance({ stack: undefined });
     expect(overdrag.stack).toBe(Overdrag.DEFAULTS.stack);
   });
 
-  it("should set props correctly", () => {
-    const props = {
-      minContentHeight: getRandomValue(),
-      minContentWidth: getRandomValue(),
-      snapThreshold: getRandomValue(),
-      controlsThreshold: getRandomValue(),
-      clickDetectionThreshold: getRandomValue(),
-      stack: Math.random() > 0.5,
-      element: elementSetup(),
-    };
-    const overdrag = createInstance(props);
-
-    expect(overdrag.minContentHeight).toBe(props.minContentHeight);
-    expect(overdrag.minContentWidth).toBe(props.minContentWidth);
-    expect(overdrag.snapThreshold).toBe(props.snapThreshold);
-    expect(overdrag.controlsThreshold).toBe(props.controlsThreshold);
-    expect(overdrag.clickDetectionThreshold).toBe(
-      props.clickDetectionThreshold
-    );
-    expect(overdrag.element).toBe(props.element);
-    expect(overdrag.parentElement).toBe(
-      props.element.offsetParent as HTMLElement
-    );
+  it(`should set "minContentHeight"`, () => {
+    const minContentHeight = getRandomValue();
+    const overdrag = createInstance({ minContentHeight });
+    expect(overdrag.minContentHeight).toBe(minContentHeight);
   });
 
-  it("should have expected defaults", () => {
+  it(`should set "minContentWidth"`, () => {
+    const minContentWidth = getRandomValue();
+    const overdrag = createInstance({ minContentWidth });
+    expect(overdrag.minContentWidth).toBe(minContentWidth);
+  });
+
+  it(`should set "maxContentHeight"`, () => {
+    const maxContentHeight = getRandomValue();
+    const overdrag = createInstance({ maxContentHeight });
+    expect(overdrag.maxContentHeight).toBe(maxContentHeight);
+  });
+
+  it(`should set "maxContentWidth"`, () => {
+    const maxContentWidth = getRandomValue();
+    const overdrag = createInstance({ maxContentWidth });
+    expect(overdrag.maxContentWidth).toBe(maxContentWidth);
+  });
+
+  it(`should set "snapThreshold"`, () => {
+    const snapThreshold = getRandomValue();
+    const overdrag = createInstance({ snapThreshold });
+    expect(overdrag.snapThreshold).toBe(snapThreshold);
+  });
+
+  it(`should set "controlsThreshold"`, () => {
+    const controlsThreshold = getRandomValue();
+    const overdrag = createInstance({ controlsThreshold });
+    expect(overdrag.controlsThreshold).toBe(controlsThreshold);
+  });
+
+  it(`should set "clickDetectionThreshold"`, () => {
+    const clickDetectionThreshold = getRandomValue();
+    const overdrag = createInstance({ clickDetectionThreshold });
+    expect(overdrag.clickDetectionThreshold).toBe(clickDetectionThreshold);
+  });
+
+  it(`should set "stack"`, () => {
+    const stack = Math.random() > 0.5;
+    const overdrag = createInstance({ stack });
+    expect(overdrag.stack).toBe(stack);
+  });
+
+  it(`should set "element"`, () => {
+    const element = elementSetup();
+    const overdrag = createInstance({ element });
+    expect(overdrag.element).toBe(element);
+  });
+
+  it(`should set "parentElement"`, () => {
+    const element = elementSetup();
+    const overdrag = createInstance({ element });
+    expect(overdrag.parentElement).toBe(element.offsetParent as HTMLElement);
+  });
+
+  it(`should set "down" as false`, () => {
     const overdrag = createInstance();
     expect(overdrag.down).toBe(false);
+  });
+
+  it(`should set "over" as false`, () => {
+    const overdrag = createInstance();
     expect(overdrag.over).toBe(false);
+  });
+
+  it(`should set "controlsActive" as false`, () => {
+    const overdrag = createInstance();
     expect(overdrag.controlsActive).toBe(false);
+  });
+
+  it(`should set "dragging" as false`, () => {
+    const overdrag = createInstance();
     expect(overdrag.dragging).toBe(false);
+  });
+
+  it(`should set "resizing" as false`, () => {
+    const overdrag = createInstance();
+    expect(overdrag.resizing).toBe(false);
   });
 
   it('should set "mouseenter" event listener on "element"', () => {
@@ -79,5 +155,57 @@ describe("Constructor", () => {
     const spy = jest.spyOn(element, "addEventListener");
     const overdrag = createInstance({ element });
     expect(spy).toHaveBeenCalledWith("mouseenter", overdrag.onMouseOver);
+  });
+
+  it(`should set element "top" style`, () => {
+    const element = elementSetup();
+    const overdrag = createInstance({ element });
+    expect(element.style.top).toBe(`${overdrag.position.fullBounds.top}px`);
+  });
+
+  it(`should set element "left" style`, () => {
+    const element = elementSetup();
+    const overdrag = createInstance({ element });
+    expect(element.style.left).toBe(`${overdrag.position.fullBounds.left}px`);
+  });
+
+  it(`should set element "right" style`, () => {
+    const element = elementSetup();
+    const overdrag = createInstance({ element });
+    expect(element.style.right).toBe(
+      `${
+        overdrag.parentPosition.actionBounds.right -
+        overdrag.position.fullBounds.right
+      }px`
+    );
+  });
+
+  it(`should set element "bottom" style`, () => {
+    const element = elementSetup();
+    const overdrag = createInstance({ element });
+    expect(element.style.bottom).toBe(
+      `${
+        overdrag.parentPosition.actionBounds.bottom -
+        overdrag.position.fullBounds.bottom
+      }px`
+    );
+  });
+
+  it(`should set element "width" style`, () => {
+    const element = elementSetup();
+    const overdrag = createInstance({ element });
+    expect(element.style.width).toBe(`${overdrag.position.width}px`);
+  });
+
+  it(`should set element "height" style`, () => {
+    const element = elementSetup();
+    const overdrag = createInstance({ element });
+    expect(element.style.height).toBe(`${overdrag.position.height}px`);
+  });
+
+  it(`should set element "position" style`, () => {
+    const element = elementSetup();
+    createInstance({ element });
+    expect(element.style.position).toBe("absolute");
   });
 });
